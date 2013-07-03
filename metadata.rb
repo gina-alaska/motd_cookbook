20ce902bc63be6ba02b204cef864992e240116e8
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-07-03 12:59:16 -0800

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             "motd"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Scott Macfarlane"
+maintainer_email "scott@gina.alaska.edu"
 license          "All rights reserved"
 description      "Installs/Configures motd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
